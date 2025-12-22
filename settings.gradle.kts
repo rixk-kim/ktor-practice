ab7bf6214786ebdcfdef422763af744e8a2348fc
rixk-kim <64937256+rixk-kim@users.noreply.github.com>
2025-12-22 15:56:37 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "ktor-practice"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.test
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
