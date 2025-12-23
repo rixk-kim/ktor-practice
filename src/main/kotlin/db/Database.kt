2f5c2745c3842054a87f4de0d8ce85964d51a9ee
rixk-kim <64937256+rixk-kim@users.noreply.github.com>
2025-12-23 10:59:26 +0900

    연습 2

--- src/main/kotlin/db/Database.kt
+++ src/main/kotlin/db/Database.kt
@@ -9,10 +9,11 @@ object Database {
     lateinit var dataSource: HikariDataSource
 
     fun init(environment: ApplicationEnvironment) {
+        val dbConfig = environment.config.config("ktor.database")
         val config = HikariConfig().apply {
-            jdbcUrl = environment.config.property("ktor.database.url").getString()
-            username = environment.config.property("ktor.database.user").getString()
-            password = environment.config.property("ktor.database.password").getString()
+            jdbcUrl = "jdbc:postgresql://${dbConfig.property("host").getString()}:${dbConfig.property("port").getString()}/${dbConfig.property("name").getString()}"
+            username = dbConfig.property("user").getString()
+            password = dbConfig.property("password").getString()
             maximumPoolSize = 10
         }
         dataSource = HikariDataSource(config)
