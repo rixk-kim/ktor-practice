6dd339db3205650b6d89c81a25243ad0b8f41726
rixk-kim <64937256+rixk-kim@users.noreply.github.com>
2025-12-31 11:07:27 +0900

    restful api 여러 명령 변경

--- src/main/kotlin/routes/Routing.kt
+++ src/main/kotlin/routes/Routing.kt
@@ -8,6 +8,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 @Serializable
@@ -21,6 +22,26 @@ fun Application.configureRouting() {
             call.respond(users)
         }
 
+        get("/users/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadGateway, "Invalid Id")
+                return@get
+            }
+
+            val user = transaction {
+                Users.select { Users.id eq id }
+                    .map { User(it[Users.id], it[Users.name])}
+                    .singleOrNull()
+            }
+
+            if (user == null) {
+                call.respond(HttpStatusCode.NotFound, "user not found")
+            } else {
+                call.respond(user)
+            }
+        }
+
         post("/users") {
             val user = call.receive<User>()
             val id = transaction {
@@ -29,6 +50,71 @@ fun Application.configureRouting() {
                 } get Users.id
             }
             call.respond(HttpStatusCode.Created, User(id, user.name))
+        }
+
+        put("/users/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid Id")
+                return@put
+            }
+
+            val user = call.receive<User>()
+            val updated = transaction {
+                Users.update({ Users.id eq id }) {
+                    it[name] = user.name
+                }
+            }
+
+            if (updated == 0) {
+                call.respond(HttpStatusCode.NotFound, "user not found")
+            } else {
+                call.respond(User(id, user.name))
+            }
+        }
+
+        delete("/users/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid Id")
+                return@delete
+            }
+
+            val deleted = transaction {
+                Users.deleteWhere { Users.id eq id }
+            }
+
+            if (deleted == 0) {
+                call.respond(HttpStatusCode.NotFound, "user not found")
+            } else {
+                call.respond(HttpStatusCode.NoContent)
+            }
+        }
+
+        patch("/users/{id}") {
+            val id = call.parameters["id"]?.toIntOrNull()
+            if (id == null) {
+                call.respond(HttpStatusCode.BadRequest, "Invalid Id")
+                return@patch
+            }
+
+            val user = call.receive<User>()
+            val updated = transaction {
+                Users.update({ Users.id eq id }) {
+                    it[name] = user.name
+                }
+            }
+
+            if (updated == 0) {
+                call.respond(HttpStatusCode.NotFound)
+            } else {
+                val updatedUser = transaction {
+                    Users.select { Users.id eq id }
+                    .map { User(it[Users.id], it[Users.name]) }
+                        .single()
+                }
+                call.respond(updatedUser)
+            }
         }
     }
 }
